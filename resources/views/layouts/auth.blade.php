bcc6a423abd0ca145311f1290dc853d1fbe7a589
Fabian ternis <f.ternis@xpsystems.eu>
2026-03-14 16:22:38 +0100

    fix: tailwind stuff ...

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? config('app.name') }}</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="h-full font-sans antialiased flex items-center justify-center px-4">
